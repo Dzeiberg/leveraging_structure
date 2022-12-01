cfcf2516471514665c61d5f0e385c88f927c57a9
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2022-12-01 14:09:07 -0500

    remove local run scripts

--- scripts/runAmazon.sh
+++
@@ -1,5 +0,0 @@
-for i in {0..9}
-do
-	echo "Running Amazon ${i}"
-	python runExperiment.py --experimentPath /data/dzeiberg/leveragingStructureResponseExperiments/experiments/amazon_reviews_pca_setting_2_$i --datasetType huggingface --problem_name amazon_reviews_multi --pca True
-done
